f92544062a0b3a0f632a1e9c8612533ba1561716
onjoroge1 <kim.njo@gmail.com>
2015-08-30 20:00:25 +0000

    changed index to show only current users notes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :notes
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
+    root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
